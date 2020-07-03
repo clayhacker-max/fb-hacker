e1fc464bf2eaf23eb57745ecd13ff1ad86a4c354
CRAZY EAM <56114046+clayhacker-max@users.noreply.github.com>
2020-07-03 08:20:37 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -20,7 +20,7 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
 
 internet = '\n\x1b[33;1m     \xe2\x95\xad\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x95\xae\n\x1b[33;1m     \xe2\x94\x83\xe2\x94\x8f\xe2\x94\x93\xe2\x94\x8f\xe2\x94\x81\xe2\x94\x81\xe2\x94\xb3\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\xb3\xe2\x94\x93\xe2\x95\xb1\xe2\x95\xb1\xe2\x95\xb1\xe2\x94\x83\x1b[32;1m Cheking Acces\n\x1b[33;1m     \xe2\x94\x83\xe2\x94\x83\xe2\x94\xa3\xe2\x94\xab\xe2\x95\xb1\xe2\x95\xb1\xe2\x97\xa2\xe2\x97\xa4\xe2\x95\xb1\xe2\x95\xb1\xe2\x94\xa3\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x83\x1b[32;1m    Internet\n\x1b[33;1m     \xe2\x94\x83\xe2\x94\x97\xe2\x94\x9b\xe2\x94\x97\xe2\x94\x81\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\xbb\xe2\x94\xbb\xe2\x94\xbb\xe2\x94\x9b\xe2\x95\xb1\xe2\x95\xb1\xe2\x95\xb1\xe2\x94\x83\n\x1b[33;1m     \xe2\x95\xb0\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x95\xaf\n\x1b[35;1m--------------------------------------'
-banner = '\n\033[31;1m____ ____ ____ ____ ___  ____ ____ _  _\n|___ |__| |    |___ |__] |  | |  | |_/\n\033[37;1m|    |  | |___ |___ |__] |__| |__| | \_\n\033[31;1m          _  _ ____ ____ _  _ ____ ____\n          |__| |__| |    |_/  |___ |__/\n\033[37;1m          |  | |  | |___ | \_ |___ |  \\\n\033[36;1mCreated By\033[31;1m :\033[32;1m Seluruh grup \033[32;1m[\033[37;1mD4RKN355 T34M\033[32;1m]\n\033[35;1m------------------------------------------'
+banner = '\n\033[31;1m____ ____ ____ ____ ___  ____ ____ _  _\n|___ |__| |    |___ |__] |  | |  | |_/\n\033[37;1m|    |  | |___ |___ |__] |__| |__| | \_\n\033[31;1m          _  _ ____ ____ _  _ ____ ____\n          |__| |__| |    |_/  |___ |__/\n\033[37;1m          |  | |  | |___ | \_ |___ |  \\\n\033[36;1mCreated By\033[31;1m :\033[32;1m Seluruh grup \033[32;1m[\033[37;1mMr.Gaming\033[32;1m]\n\033[35;1m------------------------------------------'
 
 
 def ceknet():
@@ -81,7 +81,7 @@ def start():
                         dapat.write('[ID]=> ' + email + '\n')
                         dapat.write('[PW]=> ' + pw)
                         dapat.close()
-                        print '\n\n\033[32;1m[+] \033[37;1mPASSWORD FOUND'
+                        print '\n\n\033[32;1m[+] \033[37;1mPASSWORD FOUND [Mr.Gaming]'
                         print '\033[32;1m[+] \033[37;1mUsername \033[32;1m: \033[35;1m'+email
                         print '\033[32;1m[+] \033[37;1mPassword \033[32;1m:\033[35;1m '+pw
                         print '\033[32;1m[+] \033[37;1mStatus   \033[32;1m:\033[32;1m SUCCES'
@@ -93,7 +93,7 @@ def start():
                             ceks.write('[ID]=> ' + email + '\n')
                             ceks.write('[PW]=> ' + pw)
                             ceks.close()
-                            print '\n\n\033[33;1m[+] \033[37;1mPASSWORD FOUND'
+                            print '\n\n\033[33;1m[+] \033[37;1mPASSWORD FOUND [Mr.Gaming]'
                             print '\033[33;1m[+] \033[37;1mUsername \033[32;1m: \033[35;1m'+email
                             print '\033[33;1m[+] \033[37;1mPassword \033[32;1m:\033[35;1m '+pw
                             print '\033[33;1m[+] \033[37;1mStatus   \033[32;1m:\033[33;1m CHEKPOINT'
